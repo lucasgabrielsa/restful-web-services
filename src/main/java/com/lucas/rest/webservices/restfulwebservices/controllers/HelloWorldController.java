f47d77949317cdd0cf22a841b8f4716917c56875
Lucas Gabriel <lucasgabrielsa@hotmail.com>
2019-11-16 13:19:52 -0300

    intenalizacao and swagger

--- src/main/java/com/lucas/rest/webservices/restfulwebservices/controllers/HelloWorldController.java
+++ src/main/java/com/lucas/rest/webservices/restfulwebservices/controllers/HelloWorldController.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -21,9 +22,18 @@ public class HelloWorldController {
 	public String helloWorld() {
 		return "Hello World";
 	}
+	
 	@GetMapping("/hello-internationalized")
 	public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
 		return messageSource.getMessage("good.morning.message", null, locale);
+	}
+	
+	/*
+	 * without the request header
+	 */
+	@GetMapping("/hello-internationalized-easy")
+	public String helloWorldInternationalized() {
+		return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
 	}
 	
 	@GetMapping("/hello-bean")
